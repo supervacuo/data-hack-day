c779e4ffa5a89e60fcc096df12ecd70efecd7ea8
Carl van Tonder <carl@supervacuo.com>
2012-04-03 17:22:06 -0400

    `initial` does work for CreateView.

--- central/views/response_objects.py
+++ central/views/response_objects.py
@@ -44,10 +44,11 @@ class ResponseObjectCreateView(MediaObjectMixin, EventMixin, CreateView):
 	model = ResponseObject
 	template_name = 'response_objects/add.html'
 
-	def get_form_kwargs(self):
-		kwargs = super(ResponseObjectCreateView, self).get_form_kwargs()
-		kwargs.update({'instance': ResponseObject(event=self.event, media_object=self.media_object)})
-		return kwargs
+	def get_initial(self):
+		initial = super(ResponseObjectCreateView,self).get_initial()
+		initial['event'] = self.event
+		initial['media_object'] = self.media_object
+		return initial
 
 	def dispatch(self, request, *args, **kwargs):
 		# FIXME this should not be a "global" permission
