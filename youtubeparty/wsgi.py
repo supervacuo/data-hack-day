affcbf63fea662cdb3c9d2969d2b656dcce9712e
Carl van Tonder <carl@supervacuo.com>
2012-04-04 19:06:39 -0400

    Virtualenv WSGI.

--- youtubeparty/wsgi.py
+++ youtubeparty/wsgi.py
@@ -1,21 +1,27 @@
 """
 WSGI config for youtubeparty project.
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
 """
-import os
+import os, sys
+import site
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "youtubeparty.settings")
+
+# Remember original sys.path.
+prev_sys_path = list(sys.path) 
+
+site.addsitedir('/usr/local/pythonenv/youtubeparty/lib/python2.6/site-packages/')
+
+# Reorder sys.path so new directories at the front.
+new_sys_path = []
+for item in list(sys.path):
+        if item not in prev_sys_path:
+                new_sys_path.append(item)
+                sys.path.remove(item)
+
+sys.path[:0] = new_sys_path 
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
